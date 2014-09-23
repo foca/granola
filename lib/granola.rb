4598a99edd115171acd31cd52ca0ca3daa69ed9a
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2014-09-23 06:38:21 -0700

    Granola: The documentation edition

--- lib/granola.rb
+++ lib/granola.rb
@@ -2,34 +2,72 @@ require "multi_json"
 require "granola/version"
 
 module Granola
+  # A Serializer describes how to serialize a certain type of object, by
+  # declaring the structure of JSON objects.
   class Serializer
     attr_reader :object
 
+    # Public: Instantiates a list serializer that wraps around an iterable of
+    # objects of the type expected by this serializer class.
+    #
+    # Example:
+    #
+    #   serializer = PersonSerializer.list(people)
+    #   serializer.to_json
+    #
+    # Returns a Granola::List.
     def self.list(ary)
       List.new(ary, self)
     end
 
+    # Public: Initialize the serializer with a given object.
+    #
+    # object - The domain model that we want to serialize into JSON.
     def initialize(object)
       @object = object
     end
 
+    # Public: Returns the Hash of attributes that should be serialized into
+    # JSON.
+    #
+    # Raises NotImplementedError unless you override in subclasses.
     def attributes
       fail NotImplementedError
     end
 
+    # Public: Generate the JSON string using the current MultiJson adapter.
+    #
+    # **options - Any options valid for `MultiJson.dump`.
+    #
+    # Returns a String.
     def to_json(**options)
       MultiJson.dump(attributes, options)
     end
   end
 
+  # Internal: The List serializer provides an interface for serializing lists of
+  # objects, wrapping around a specific serializer.
+  #
+  # Example:
+  #
+  #   serializer = Granola::List.new(people, PersonSerializer)
+  #   serializer.to_json
+  #
+  # You should use Serializer.list instead of this class.
   class List < Serializer
+    # Internal: Get the serializer class to use for each item of the list.
     attr_reader :item_serializer
 
+    # Public: Instantiate a new list serializer.
+    #
+    # list       - An Array-like structure.
+    # serializer - A subclass of Granola::Serializer.
     def initialize(list, serializer)
       @item_serializer = serializer
       @list = list.map { |obj| serializer.new(obj) }
     end
 
+    # Public: Returns an Array of Hashes that can be serialized into JSON.
     def attributes
       @list.map(&:attributes)
     end
--- lib/granola/helper.rb
+++ lib/granola/helper.rb
@@ -1,17 +1,41 @@
 require "granola"
 
 module Granola::Helper
+  # Public: Returns the serializer object for rendering a specific object. The
+  # class will attempt to be inferred based on the class of the passed object,
+  # but a specific serializer can be passed via a keyword argument `with`.
+  #
+  # object - The Object to serialize.
+  #
+  # Keywords
+  #   with: A specific serializer class to use. If this is `nil`,
+  #         `Helper.serializer_class_for` will be used to infer the serializer
+  #         class.
+  #
+  # Raises NameError if no specific serializer is provided and we fail to infer
+  #   one for this object.
+  # Returns an instance of a Granola::Serializer subclass.
   def serializer_for(object, with: nil)
     serializer_class = with || Granola::Helper.serializer_class_for(object)
     method = object.respond_to?(:to_ary) ? :list : :new
     serializer_class.send(method, object)
   end
 
+  # Internal: Infers the name of a serialized based on the class of the passed
+  # object. The pattern is the Object's class + "Serializer". So
+  # `PersonSerializer` for `Person`.
+  #
+  # object - An object of a class with a matching serializer.
+  #
+  # Raises NameError if no matching class exists.
+  # Returns a Class.
   def self.serializer_class_for(object)
     object = object.respond_to?(:to_ary) ? object.to_ary.fetch(0, nil) : object
     const_get("#{object.class.name}Serializer")
   end
 
+  # Internal: Null serializer that transparently handles rendering `nil` in case
+  # it's passed.
   class NilClassSerializer < Granola::Serializer
     def attributes
       {}
